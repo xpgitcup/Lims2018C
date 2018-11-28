e9fd408676ab3354365433c8c095f5c7f0b53482
LiXiaoping <xplisina@sina.com>
2018-11-28 16:04:15 +0800

    开始初始化

--- grails-app/init/lims2018c/BootStrap.groovy
+++ grails-app/init/lims2018c/BootStrap.groovy
@@ -2,21 +2,27 @@ package lims2018c
 
 import org.omg.CORBA.Environment
 
+import java.util.logging.Logger
+
 class BootStrap {
 
     def initService
     def grailsApplication
 
     def init = { servletContext ->
-        environments { e->
-            switch (Environment.name) {
-                case grails.util.Environment.DEVELOPMENT:
-                    break
-                case grails.util.Environment.PRODUCTION:
-                    break
+        Logger LOG = Logger.getLogger(this.class.getName());
+        environments {
+            development {
+                println("开发环境...")
+                initService.configureForDevelopment(servletContext);
+            }
+            production {
+                println("发布环境...")
+                initService.configureForDevelopment(servletContext);
             }
         }
     }
+
     def destroy = {
     }
 }
--- grails-app/services/cn/edu/cup/init/InitService.groovy
+++ grails-app/services/cn/edu/cup/init/InitService.groovy
@@ -2,10 +2,104 @@ package cn.edu.cup.init
 
 import grails.gorm.transactions.Transactional
 
+import javax.servlet.ServletContext
+
 @Transactional
 class InitService {
 
-    def serviceMethod() {
+    def dataSource
+    def dataDictionaryService
+    def systemMenuService
+    def dataKeyService
+    def dataItemService
+    def commonService
 
+    /**
+     * 初始化代码__开发环境下的初始化代码
+     */
+    def configureForDevelopment(ServletContext servletContext) {
+        println "这是开发环境..."
+        def webRootDir = servletContext.getRealPath("/")
+        def configFileName = "${webRootDir}config.ini"
+        def configFile = new File(configFileName)
+        def config = new Properties()
+        if (configFile.exists()) {
+            println("读取配置文件.")
+            def inf = new FileInputStream(configFile)
+            def reader = new InputStreamReader(inf, "UTF-8")
+            config.load(reader)
+            def scriptsTemp = config.getProperty("scripts")
+            println(scriptsTemp)
+            if (scriptsTemp) {
+                println("脚本文件：" + scriptsTemp)
+                def scripts = scriptsTemp.split(",")
+                scripts.each { e ->
+                    loadScripts("${webRootDir}${e}")
+                }
+            }
+        } else {
+            def of = new FileOutputStream(configFile, false)
+            def ow = new OutputStreamWriter(of, "utf-8")
+            config.setProperty("scripts", "文件1,文件2")
+            //config.storeToXML(of, "系统配置文件", "utf-8")
+            config.store(ow, "系统配置文件")
+            println("创建配置文件.")
+        }
     }
+
+    /**
+     * 发布后的初始化代码
+     */
+    def configureForProduction(ServletContext servletContext) {
+        println "这是发布环境..."
+        def webRootDir = servletContext.getRealPath("/")
+        def scriptPath = "${webRootDir}scripts/system"
+        println "BootStrap ${webRootDir}"
+        initService.loadScripts(scriptPath)
+    }
+
+    /*
+    * 加载数据库初始化脚本
+    * */
+
+    def loadScripts(String dir) {
+        File sf = new File(dir)
+        if (sf.exists()) {
+            println "load scripts ${dir}"
+            if (sf.isDirectory()) {
+                sf.eachFile { f ->
+                    if (f.isFile()) {
+                        executeScript(f)
+                    }
+                }
+            } else {
+                println("执行${sf}...")
+                executeScript(sf)
+            }
+        }
+    }
+
+    /**
+     * 执行附加脚本
+     * */
+
+    def executeScript(File sf) {
+        //def File sf = new File(fileName)
+        println "init - ${sf}"
+        if (sf) {
+            def db
+            def sql = sf.text
+            db = new groovy.sql.Sql(dataSource)
+            //println "init - ${sql}"
+            def lines = sql.split(";")
+            lines.each() { it ->
+                //println "line: ${it}"
+                it = it.trim()
+                if (!it.isEmpty()) {
+                    db.executeUpdate(it)
+                }
+            }
+        }
+    }
+
 }
